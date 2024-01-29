77a63198a12a41011c2ce6d54f125f8cfbc5539d
Feeco <lifeike@gmail.com>
2024-01-29 09:18:57 -0700

    modify blogs page layout

--- layouts/partials/Posts.js
+++ layouts/partials/Posts.js
@@ -7,46 +7,50 @@ const Posts = ({ posts }) => {
   const { blog_folder, summary_length } = config.settings;
   return (
     <div className="section row pb-0">
-      <div className="col-12 pb-12 lg:pb-24">
-        <div className="row items-center">
-          <div className="col-12 md:col-6">
-            {posts[0].frontmatter.image && (
-              <Image
-                className="h-auto w-full rounded-lg"
-                src={posts[0].frontmatter.image}
-                alt={posts[0].frontmatter.title}
-                width={540}
-                height={227}
-                priority={true}
-              />
-            )}
+      {posts.map((item, i) => {
+        return (
+          <div className="col-12 pb-12 lg:pb-24">
+            <div className="row items-center">
+              <div className="col-12 md:col-6">
+                {posts[i].frontmatter.image && (
+                  <Image
+                    className="h-auto w-full rounded-lg"
+                    src={posts[i].frontmatter.image}
+                    alt={posts[i].frontmatter.title}
+                    width={540}
+                    height={227}
+                    priority={true}
+                  />
+                )}
+              </div>
+              <div className="col-12 md:col-6">
+                <h2 className="h3 mb-2 mt-4">
+                  <Link
+                    href={`/${blog_folder}/${posts[i].slug}`}
+                    className="block hover:text-primary"
+                  >
+                    {posts[i].frontmatter.title}
+                  </Link>
+                </h2>
+                <p className="text-text">
+                  {plainify(
+                    posts[i].content?.slice(i, Number(summary_length)),
+                    "div"
+                  )}
+                </p>
+                <Link
+                  className="btn btn-primary mt-4"
+                  href={`/${blog_folder}/${posts[i].slug}`}
+                  rel=""
+                >
+                  Read More
+                </Link>
+              </div>
+            </div>
           </div>
-          <div className="col-12 md:col-6">
-            <h2 className="h3 mb-2 mt-4">
-              <Link
-                href={`/${blog_folder}/${posts[0].slug}`}
-                className="block hover:text-primary"
-              >
-                {posts[0].frontmatter.title}
-              </Link>
-            </h2>
-            <p className="text-text">
-              {plainify(
-                posts[0].content?.slice(0, Number(summary_length)),
-                "div"
-              )}
-            </p>
-            <Link
-              className="btn btn-primary mt-4"
-              href={`/${blog_folder}/${posts[0].slug}`}
-              rel=""
-            >
-              Read More
-            </Link>
-          </div>
-        </div>
-      </div>
-      {posts.slice(1).map((post, i) => (
+        );
+      })}
+      {/* {posts.slice(1).map((post, i) => (
         <div key={`key-${i}`} className="col-12 mb-8 sm:col-6 lg:col-4">
           {post.frontmatter.image && (
             <Image
@@ -74,7 +78,7 @@ const Posts = ({ posts }) => {
             Read More
           </Link>
         </div>
-      ))}
+      ))} */}
     </div>
   );
 };
